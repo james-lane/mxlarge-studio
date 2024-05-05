fa887b3fa879f62c5eb47243d911c42d763704bc
james-lane <4426937+james-lane@users.noreply.github.com>
2024-05-05 19:52:06 +0100

    Update config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,35 @@
 import {defineConfig} from 'sanity'
-import { structureTool } from "sanity/structure";
+import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import {media} from 'sanity-plugin-media'
+import {RobotIcon, RocketIcon} from '@sanity/icons'
 
-export default defineConfig({
-  name: 'default',
-  title: 'mxlarge',
+export default defineConfig([
+  {
+    projectId: '8oyzogbt',
+    name: 'Development',
+    title: 'Development',
+    icon: RobotIcon,
+    basePath: '/development',
+    dataset: 'stub',
+    plugins: [structureTool(), visionTool(), media()],
 
-  projectId: '8oyzogbt',
-  dataset: 'stub',
-
-  plugins: [structureTool(), visionTool(), media()],
-
-  schema: {
-    types: schemaTypes,
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    projectId: '8oyzogbt',
+    name: 'Production',
+    title: 'Production',
+    icon: RocketIcon,
+    basePath: '/production',
+    dataset: 'production',
+    plugins: [structureTool(), visionTool(), media()],
+
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
