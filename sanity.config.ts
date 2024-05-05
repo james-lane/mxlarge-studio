514b1963362de8dcfb7fac66f2b7b8444a98d1ae
james-lane <4426937+james-lane@users.noreply.github.com>
2024-05-05 19:39:22 +0100

    Upgrade sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,9 +1,8 @@
 import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import { structureTool } from "sanity/structure";
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 import {media} from 'sanity-plugin-media'
-import {scheduledPublishing} from '@sanity/scheduled-publishing'
 
 export default defineConfig({
   name: 'default',
@@ -12,7 +11,7 @@ export default defineConfig({
   projectId: '8oyzogbt',
   dataset: 'stub',
 
-  plugins: [deskTool(), visionTool(), media(), scheduledPublishing()],
+  plugins: [structureTool(), visionTool(), media()],
 
   schema: {
     types: schemaTypes,
